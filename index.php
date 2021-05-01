32a0e49600924eddbf982249b758fecc56820379
youmeSon <rosydarling@naver.com>
2021-05-01 20:36:40 +0100

    Add validation for phone & email

--- index.php
+++ index.php
@@ -1,12 +1,35 @@
+
 <?php
 $name = $_POST["name"];
-$phone = $_POST["phone"];
-$message = $_POST["message"];
+$mobileregex = "/^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/";
 
-$db = mysqli_connect("localhost", "root", "000000", "mysql");
-$query = "INSERT INTO form_data (name, phone, message) VALUES ('$name', '$phone', '$message')";
-mysqli_query($db, $query);
+$success_message = "Thanks, $name!. Your message was submitted successfully!";
+$error_message = "* There was an error with your submition. Please try again.";
 
-echo "Thanks, $name!. Your message was submitted successfully!";
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
+  $phone = $_POST["phone"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  if(preg_match($mobileregex, $phone) == true ) {
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $db = mysqli_connect("localhost", "root", "000000", "mysql");
+      $query = "INSERT INTO form_data (name, phone, email, message) VALUES ('$name', '$phone', '$email, '$message')";
+      mysqli_query($db, $query);
+
+      echo $success_message;
+    } else {
+      echo "Wrong email form! ".$error_message;
+    }
+  } else {
+    echo "Wrong phone number form!".$error_message;
+  }
+}
+
+
+
+
+
 
 ?>
